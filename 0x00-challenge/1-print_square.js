c857e437798037c19d7b52a047ee8b31709c29a0
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-07-30 22:45:25 +0000

    rest task

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,19 +1,24 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
 
-function printSquare(size) {
-    for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) { // <-- Fixed the condition here
-            process.stdout.write('#');
-        }
-        console.log('');
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
     }
+    process.stdout.write("\n");
 }
-
-if (process.argv.length !== 3) {
-    console.log('Usage: ./1-print_square.js <size>');
-    process.exit(1);
-}
-
-var size = parseInt(process.argv[2]);
-printSquare(size);
-
